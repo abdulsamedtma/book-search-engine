69e252f829b2fc6f075094700feab327f20a18e0
Abdul Samed Alhassan <129804994+abdulsamedtma@users.noreply.github.com>
2023-11-13 21:43:48 -0600

    Imported modules, components and added login_user mutation hook

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,24 +1,31 @@
-// see SignupForm.js for comments
+// Import necessary modules and components
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
+// Define the LoginForm component
 const LoginForm = () => {
+  // State to manage user form data and form validation
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
+  // UseMutation hook for the LOGIN_USER mutation
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  // Handle input changes in the form fields
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // Handle form submission
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
+    // Check form validity
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -26,33 +33,36 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // Execute the loginUser mutation
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // Log in the user if the mutation is successful
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
+      // Show an alert if there's an error with login credentials
       setShowAlert(true);
     }
 
+    // Clear form data
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
   };
 
+  // Render the login form
   return (
     <>
+      {/* Display an alert for login errors */}
+      <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+        Something went wrong with your login credentials!
+      </Alert>
+
+      {/* Render the login form using react-bootstrap components */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
         <Form.Group className='mb-3'>
           <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
@@ -78,6 +88,8 @@ const LoginForm = () => {
           />
           <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
         </Form.Group>
+
+        {/* Submit button */}
         <Button
           disabled={!(userFormData.email && userFormData.password)}
           type='submit'
@@ -89,4 +101,5 @@ const LoginForm = () => {
   );
 };
 
+// Export the LoginForm component
 export default LoginForm;
