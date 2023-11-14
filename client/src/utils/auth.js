d72b990571d8bf78285bc25550f2dfe00b45ff8f
Abdul Samed Alhassan <129804994+abdulsamedtma@users.noreply.github.com>
2023-11-13 22:27:37 -0600

    Add comment to improve code readability

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,14 +1,14 @@
-// use this to decode a token and get the user's information out of it
+// This can be used to decode a token and get the user's information accordingly:
 import decode from 'jwt-decode';
 
-// create a new class to instantiate for a user
+// Create a new class to instantiate for a user
 class AuthService {
   // get user data
   getProfile() {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
+  // Check if user's logged in
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
